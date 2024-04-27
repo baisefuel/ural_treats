f02d77e35db07502b077bac99087a5f9ae3af2a4
baisefuel <s.bril03@gmail.com>
2024-04-27 21:38:43 +0500

    1.1

--- ural_treats/ural_treats/urls.py
+++ ural_treats/ural_treats/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from views import index_view
+from django.conf import settings
+
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='index'),
+    path('', views.index_view, name='index'),
 ]
