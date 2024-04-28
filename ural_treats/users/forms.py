73c44611897d08ec63163c2e3df33f50d5e49684
baisefuel <s.bril03@gmail.com>
2024-04-28 21:41:24 +0500

    1.3

--- ural_treats/users/forms.py
+++ ural_treats/users/forms.py
@@ -15,6 +15,13 @@ class UserLoginForm(AuthenticationForm):
 
 class UserRegistrationForm(UserCreationForm):
 
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
     class Meta:
         model = User
         fields = (
@@ -24,13 +31,6 @@ class UserRegistrationForm(UserCreationForm):
             "email",
             "password1",
         )
-    
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    username = forms.CharField()
-    email = forms.CharField()
-    password1 = forms.CharField()
-    password2 = forms.CharField()
 
 class ProfileForm(UserChangeForm):
     class Meta:
