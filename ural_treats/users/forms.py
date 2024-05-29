d313f02b6cd854a9fbe559273393f54385933e4a
baisefuel <s.bril03@gmail.com>
2024-05-30 02:57:13 +0500

    3.0

--- ural_treats/users/forms.py
+++ ural_treats/users/forms.py
@@ -1,22 +1,25 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
-from users.models import User
-
+from .models import User
 
 class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(label='Имя')
-    last_name = forms.CharField(label='Фамилия')
-    username = forms.CharField(label='Имя пользователя')
-    email = forms.EmailField(label='Email')
-    password1 = forms.CharField(label='Пароль')
-    password2 = forms.CharField(label='Подтверждение пароля', help_text='Введите тот же пароль, что и выше, для проверки.')
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-input mail', 'placeholder': 'Ваша почта'})),
+    tel = forms.TextInput(attrs={'class': 'form-input tel', 'placeholder': 'Телефон'}),
+
 
     class Meta:
         model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "username",
-            "email",
-        )
+        fields = ['last_name', 'first_name', 'email', 'tel', 'password1', 'password2']
+        widgets = {
+            'last_name': forms.TextInput(attrs={'class': 'form-input lastName', 'placeholder': 'Фамилия'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-input name', 'placeholder': 'Имя'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-input pass', 'placeholder': 'Пароль'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-input pass_again', 'placeholder': 'Подтверждение пароля'}),
+        }
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- ural_treats/users/models.py
+++ ural_treats/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
+    tel = models.CharField(max_length=15, blank=True, null=True, verbose_name="Телефон")
+
     class Meta:
         db_table = 'user'
-        verbose_name = 'Пользователя'
+        verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
